c8add3ff48f4b0dd92b8699f74f05df9e30e268c
Enrico Crema <enrico.crema@gmail.com>
2015-03-05 19:13:53 +0100

    Bug fixed, .RData updated
    Parameter name changes v —>sigma
    Parameter removed : b

--- submitLegion/prepareSweep.R
+++ submitLegion/prepareSweep.R
@@ -5,29 +5,27 @@
 
 # Holding z constant #
 library(tgp)
-v=c(0,3)
-b=c(1)
+sigma=c(0,3)
 z=c(1)
 mu=c(0.005)
 k=c(1,500)
 number.sim.per.job=3
 number.jobs=10000
-paramsweep<-lhs(number.sim.per.job*number.jobs,rect=rbind(v,b,z,mu,k))
-colnames(paramsweep)<-c("v","b","z","mu","k")
-paramsweep[,5]<-round(paramsweep[,5])
+paramsweep<-lhs(number.sim.per.job*number.jobs,rect=rbind(sigma,z,mu,k))
+colnames(paramsweep)<-c("sigma","z","mu","k")
+paramsweep[,4]<-round(paramsweep[,4])
 save(paramsweep,file="./submitLegion/sweep1.RData")
 
 # Sweeping z #
-v=c(0,3)
-b=c(1)
+sigma=c(0,3)
 z=c(0,1)
 mu=c(0.005)
 k=c(1,500)
 number.sim.per.job=3
 number.jobs=10000
-paramsweep<-lhs(number.sim.per.job*number.jobs,rect=rbind(v,b,z,mu,k))
-colnames(paramsweep)<-c("v","b","z","mu","k")
-paramsweep[,5]<-round(paramsweep[,5])
+paramsweep<-lhs(number.sim.per.job*number.jobs,rect=rbind(sigma,z,mu,k))
+colnames(paramsweep)<-c("sigma","z","mu","k")
+paramsweep[,4]<-round(paramsweep[,4])
 save(paramsweep,file="./submitLegion/sweep2.RData")
 
 
